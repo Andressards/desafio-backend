eeaba8c36558a6bab040d8bed851b6d26580951a
Andressards <andressarodrigues.profissional@gmail.com>
2026-01-08 14:37:15 -0300

    feat: testes de integração e unitários

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\TransferService;
+use App\Http\Requests\TransferRequest;
 
 class TransferController extends Controller
 {
-    public function store(Request $request, TransferService $transferService)
+    public function store(TransferRequest $request, TransferService $transferService)
     {
-        $transfer = $transferService->execute($request->all());
+        $transfer = $transferService->execute($request->validated());
 
         return response()->json($transfer, 201);
     }
